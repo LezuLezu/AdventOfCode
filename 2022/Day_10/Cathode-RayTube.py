cd8b98e600093c5130b9b06221b3909cd66dab07
Zoë <zoe.vandijk@casema.nl>
2022-12-12 18:04:33 +0100

    finished day 10 2022

--- 2022/Day_10/Cathode-RayTube.py
+++ 2022/Day_10/Cathode-RayTube.py
@@ -5,11 +5,8 @@ def getData():
         data = [line.strip().split(" ") for line in file]     
     for line in data:
         if len(line) == 2:
-            # print(line)
-            if line[1].startswith("-"):
-                line[1] = ["-", int(line[1][1:])]
-            else:
-                line[1] = ["+", int(line[1])]
+            line[1] = int(line[1])
+            
     # print(data)
     return data
 
@@ -22,34 +19,54 @@ def getSignalStrength(data):
     for line in data:
         if line[0] == "noop":
             cycleCount += 1
-        if line[0] == "addx":
+        elif line[0] == "addx":
             cycleCount += 1
             if cycleCount in cycleCountStrenghtList:
                 # print(cycleCount * register)
                 sigalStrengthList.append(cycleCount * register)
-            if line[1][0] == "+":
-                register += line[1][1]
-                cycleCount += 1
-            if line[1][0] == "-":
-                register -= line[1][1]
-                cycleCount += 1
+            register += line[1]
+            cycleCount += 1
         if cycleCount in cycleCountStrenghtList:
-            sigalStrengthList.append(cycleCount * register)
-
-
-    # print(sigalStrengthList)
-    # print(sum(sigalStrengthList))
+                # print(cycleCount * register)
+                sigalStrengthList.append(cycleCount * register)
     return sum(sigalStrengthList)
 
-# def grid(data):
+def fillGrid(grid, cycleCount, register):
+    # print(cycleCount)
+    # print(register)
+    if abs((cycleCount % 40) - register) <= 1:
+        # print("in grid draw")
+        grid[max(0, cycleCount // 40)][min(40, cycleCount % 40)] = "#"
+    return grid
 
+def grid(data):
+    cycleCount = 0
+    register = 1
+    gridWith = 40
+    gridHeight = 6
+    grid = [["." for x in range(gridWith)] for y in range(gridHeight)]
 
-            
-
+    for line in data:
+        grid = fillGrid(grid, cycleCount, register)
+        if line[0] == "noop":
+            cycleCount += 1
+        elif line[0] == "addx":
+            cycleCount += 1
+            grid = fillGrid(grid, cycleCount, register)
+            register += line[1]
+            cycleCount += 1
+        grid = fillGrid(grid, cycleCount, register)
+    return grid
 
 def main():
     data = getData()
-    # print(data)
+
     p1 = getSignalStrength(data)
     print("Part One: Sum of six signal Strengths is %s" %(p1))
+
+    p2 = grid(data)
+    print('Part 2\n')
+    for line in p2:
+        print(''.join(line))
+
 main()
